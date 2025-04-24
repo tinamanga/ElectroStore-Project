4000ea92c809c6955346a580be6665c0e43f5f94
tinamanga <tinamanga@gmail.com>
2025-04-24 13:06:29 +0300

    Updated product layout

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 
+
 const ProductList = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/db.json'); // Fetch from the public directory
+      const response = await fetch("/db.json"); // Fetch from public directory
       const data = await response.json();
-      setProducts(data.products); // Set the products in state
+      setProducts(data.products);
     };
     fetchData();
   }, []);
@@ -16,12 +17,16 @@ const ProductList = () => {
   if (!products.length) return <div>Loading...</div>;
 
   return (
-    <div>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className="product-list-container">
+      <h2>Our Products</h2>
+      <div className="product-grid">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default ProductList;
+
