484218c5322ca376928b891c6fa252d78e6e9bd8
tinamanga <tinamanga@gmail.com>
2025-04-25 23:44:10 +0300

    Added Footer and AddProduct button

--- src/components/dashboard/DashboardFooter.jsx
+++ src/components/dashboard/DashboardFooter.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="dashboard-footer">
-        <p>&copy; {new Date().getFullYear()} ElectroStore. All rights reserved. Designed by Christina</p>
+        <p>&copy; {new Date().getFullYear()} ElectroStore. All rights reserved.</p>
       </footer>
     );
   }
